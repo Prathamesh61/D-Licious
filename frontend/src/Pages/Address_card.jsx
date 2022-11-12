ec18f101ee0158cb234373926c2ccdd6821df94d
Prathamesh61 <rawoolprathamesh61@gmail.com>
2022-11-12 22:18:58 +0530

    Profile Functions,Address,cartDrawer

--- frontend/src/Pages/Address_card.jsx
+++ frontend/src/Pages/Address_card.jsx
@@ -1,13 +1,14 @@
 import { Heading, HStack, Image, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
-const Address_card = () => {
+const Address_card = ({ item }) => {
     return (
         <HStack border={"1px solid black"} borderRadius={"5px"} padding="10px" justifyContent={"start"} alignItems={"start"}>
             <Image width={"25px"} src='https://www.licious.in/img/rebranding/elements/house.jpg' alt='location' />
             <VStack>
-                <Text alignSelf={"start"} fontSize={"13px"} lineHeight={"13px"}>B101 VANITA SOC SHANKESHWAR NAGAR ACHOLE ROAD NALLASOPARA</Text>
-                <Text alignSelf={"start"} fontSize={"13px"} lineHeight={"13px"}>(EAST),</Text>
-                <Text alignSelf={"start"} fontSize={"13px"} lineHeight={"13px"}>Nala Sopara - 401203, Maharashtra</Text>
+                <Text alignSelf={"start"} fontSize={"13px"} lineHeight={"13px"}>{item.bldgno}</Text>
+                <Text alignSelf={"start"} fontSize={"13px"} lineHeight={"13px"}>{item.locality}</Text>
+                <Text alignSelf={"start"} fontSize={"13px"} lineHeight={"13px"}>{item.landmark}</Text>
+                <Text alignSelf={"start"} fontSize={"13px"} lineHeight={"13px"}>{item.city}</Text>
             </VStack>
         </HStack>
     )
